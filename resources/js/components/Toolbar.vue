6f40b1194c0ac80bae57572311136ae0f8f26eff
bibek12 <sbibek100@gmail.com>
2019-10-11 22:01:09 +0545

    Veutify Vue Router and Initial Components

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Bibek Forum</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Forum</v-btn>
+      <v-btn flat>Ask Category</v-btn>
+      <v-btn flat>Category</v-btn>
+      <v-btn flat>Login</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
